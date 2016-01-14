38e1374ff518f5303a497b54b628070e0a120dfc
felippemr <raposo.felippe@gmail.com>
2016-01-14 00:47:06 -0200

    Add has_anomaly tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
-
+#[warn(unused_variables)]
+pub fn has_anomaly(timestamp_array: &Vec<(&str, i32)>) -> bool<> {
+    return true;
+}
--- tests/unit.rs
+++ tests/unit.rs
@@ -1,5 +1,9 @@
+extern crate heimdall;
+
 #[test]
+fn timestamp_array_has_anomaly(){
+    let timestamp_array = vec![("100001", 1), ("100002", 1),
+                               ("100003", 1), ("100004", 2)];
 
-fn it_works(){
-
+    assert!(heimdall::has_anomaly(&timestamp_array), true);
 }
