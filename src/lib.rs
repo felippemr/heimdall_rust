ec35ebdeacc75145e35ec65ad9f5444919580b4f
felippemr <raposo.felippe@gmail.com>
2016-01-14 17:58:33 -0200

    Change heimdall: improve 'previous' var name

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 pub fn has_anomaly(datapoint_vector: &Vec<(i32, f32)>) -> bool{
     for datapoint in datapoint_vector.iter().enumerate() {
-        let previous: f32 = match datapoint.0 {
+        let previous_datapoint: f32 = match datapoint.0 {
             0 => (datapoint.1).1,
             _ => datapoint_vector[datapoint.0 - 1].1
         };
-        if previous != (datapoint.1).1 {
+        if previous_datapoint != (datapoint.1).1 {
             return true;
         };
     };
