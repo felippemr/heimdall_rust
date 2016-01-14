3914f6bcfd1ca073e57c80c0c711396c3f63cda9
felippemr <raposo.felippe@gmail.com>
2016-01-14 16:11:53 -0200

    Change has_anomaly: improve algorithm and test non_anomalous case

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-#[warn(unused_variables)]
-pub fn has_anomaly(timestamp_array: &Vec<(&str, i32)>) -> bool<> {
-    return true;
+pub fn has_anomaly(datapoint_vector: &Vec<(i32, f32)>) -> bool{
+    for datapoint in datapoint_vector.iter().enumerate() {
+        let previous = match datapoint.0 {
+            0 => (datapoint.1).1,
+            _ => { datapoint_vector[datapoint.0 - 1].1 }
+        };
+        if previous != (datapoint.1).1 {
+            return true;
+        };
+    };
+    return false;
 }
--- tests/unit.rs
+++ tests/unit.rs
@@ -2,8 +2,14 @@ extern crate heimdall;
 
 #[test]
 fn timestamp_array_has_anomaly(){
-    let timestamp_array = vec![("100001", 1), ("100002", 1),
-                               ("100003", 1), ("100004", 2)];
+    let a_datapoint_vector = vec![(1452776170, 1.1), (1452776171, 1.1),
+                               (1452776172, 1.1), (1452776173, 2.1)];
+    assert_eq!(heimdall::has_anomaly(&a_datapoint_vector), true);
+}
 
-    assert!(heimdall::has_anomaly(&timestamp_array), true);
+#[test]
+fn timestamp_array_does_not_has_anomaly(){
+    let a_datapoint_vector = vec![(1452776170, 1.1), (1452776171, 1.1),
+                               (1452776172, 1.1), (1452776173, 1.1)];
+    assert_eq!(heimdall::has_anomaly(&a_datapoint_vector), false);
 }
