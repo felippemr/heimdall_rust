1f4e46733e46c4937ab212231089e299df40bb9f
felippemr <raposo.felippe@gmail.com>
2016-01-14 17:42:23 -0200

    Change heimdall: remove unused brackets

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub fn has_anomaly(datapoint_vector: &Vec<(i32, f32)>) -> bool{
     for datapoint in datapoint_vector.iter().enumerate() {
         let previous: f32 = match datapoint.0 {
             0 => (datapoint.1).1,
-            _ => { datapoint_vector[datapoint.0 - 1].1 }
+            _ => datapoint_vector[datapoint.0 - 1].1
         };
         if previous != (datapoint.1).1 {
             return true;
